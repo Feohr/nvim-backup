34c94855c6afd7d0487cabbff6fcee6b5158ccc9
Rehaan Mohammed <mohammedrehaan.work@gmail.com>
2023-08-15 01:00:29 +0530

    Change arrow symbol

--- lua/nvimreq.lua
+++ lua/nvimreq.lua
@@ -16,8 +16,8 @@ require('nvim-tree').setup {
         icons = {
             glyphs = {
                 folder = {
-                    arrow_closed = '⮞',
-                    arrow_open = '⮟'
+                    arrow_closed = '→',
+                    arrow_open = '←'
                 },
             },
         },
